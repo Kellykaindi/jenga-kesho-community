9576611a811859a13360367c34a951b2948a529e
Kelly Mbai <mbaikellyk@gmail.com>
2025-03-30 22:58:58 +0300

    first commit

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+    <style>
+        body{
+            background-image: url('jkcindex.gif');  
+            background-size: cover; 
+            background-repeat: no-repeat;
+            background-position: center center; 
+        } 
+    </style>
+    
+</html>
